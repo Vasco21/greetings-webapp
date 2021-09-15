01b31b39f13470be040e087c296f7c495916787b
Vasco Eti <vascoeti123@gmail.com>
2021-09-15 12:50:52 +0200

    update

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-var dbcon = process.env.DATABASE_URL || 'postgresql:greetings:vasco21@localhost:5432/greetings'
+var dbcon = process.env.DATABASE_URL || 'postgresql:postgres:vasco21@localhost:5432/greetings'
 const pool = new Pool({
         connectionString: dbcon,
         ssl:{
